91641d6273dcb8610e92c8b8a69df6fe6a012392
Ron Lawrence <ron@linkertech.com>
2018-07-04 23:02:24 -0600

    "functional" plugin.  - controls added to div with power(laser), off, on (cnc and laser).  - Gcodes sent on button clicks
     TODO: need to send gcode on slider (laser).

--- octoprint_snapmaker_control/static/js/snapmaker_control.js
+++ octoprint_snapmaker_control/static/js/snapmaker_control.js
@@ -13,70 +13,84 @@ $(function () {
     self.cnc = ko.observable(undefined);
     self.fileLoaded = ko.observable(undefined);
     self.showEntry = ko.observable(undefined);
+    self.fileLoaded = ko.observable(undefined);
+    self.toolPercentage = ko.observable(undefined);
+    self.toolStatus = ko.observable(undefined);
 
     // assign the injected viewModel parameters
     self.connectionViewModel = parameters[0];
     self.navigationViewModel = parameters[1];
     self.settingsViewModel = parameters[2];
 
+    self.toolPercentage(10);
+
     //---------  functions  ----------------
-    self.onEventConnected = function(payload) {
+    self.onEventConnected = function (payload) {
       console.log('connected', payload);
-      self.cnc(false);
-      self.laser(false);
-      self.showEntry(false);
-      self.detachControls();
-      self.fileLoaded(false);
+      self.allOff();
       setTimeout(self.getSnapmakerInfo, 2000);
     };
 
-    self.onEventDisconnected = function(payload) {
-      console.log('Disconnected', payload);
+    self.allOff = function () {
       self.cnc(false);
       self.laser(false);
       self.showEntry(false);
-      self.detachControls();
+    };
+
+    self.onEventDisconnected = function (payload) {
+      console.log('Disconnected', payload);
+      self.allOff();
     };
 
     self.selectAFunction = function (func) {
       console.log('Function: ', func);
     };
 
-    self.detachControls = function () {
-      var snapmaker=$("#snapmaker_controls");
-      if (snapmaker) {
-        snapmaker.detach();
+    self.sendOnCommand = function () {
+      if (self.cnc()) {
+        console.log('ON');
+        OctoPrint.control.sendGcode("M3");
+      } else {
+        console.log('ON', self.toolPercentage());
+        var pct = Math.trunc(self.toolPercentage());
+        var pct255 = Math.trunc(self.toolPercentage() * 255 / 100);
+        OctoPrint.control.sendGcode("M3 P"+ pct + " S" + pct255);
       }
     };
 
+    self.sendOffCommand = function () {
+      console.log('OFF');
+      OctoPrint.control.sendGcode("M5");
+    };
+
     self.attachControls = function () {
-      var snapmaker=$("#snapmaker_controls");
+      var snapmaker = $("#snapmaker_controls");
       if (snapmaker) {
-        if (snapmaker.parent() !== self.customControlsDiv) {
-          self.customControlsDiv.append(snapmaker);
-        }
+        // $("#control").after(snapmaker);
+        ko.cleanNode(snapmaker[0]);
+        $("#control-jog-custom").after(snapmaker);
+        ko.applyBindings(self, snapmaker[0]);
+      } else {
+        console.error('Unable to get snapmaker_controls template!');
       }
     };
 
     self.getSnapmakerInfo = function () {
       var url = OctoPrint.options.baseurl;
       console.log('calling ', OctoPrint.options);
-      OctoPrint.get('plugin/snapmaker_control/status').then(function (response) {
-        console.log('Snapmaker Status', response);
-        self.firmware = response.firmware;
-        self.tool = response.tool;
-        if (response.status === true) {
-          self.cnc(response.cnc);
-          self.laser(response.laser);
-          self.showEntry(true);
-          self.attachControls();
-        } else {
-          self.cnc(false);
-          self.laser(false);
-          self.showEntry(false);
-          self.detachControls();
-        }
-      });
+      OctoPrint.get('plugin/snapmaker_control/status')
+        .then(function (response) {
+          console.log('Snapmaker Status', response);
+          self.firmware = response.firmware;
+          self.tool = response.tool;
+          if (response.status === true) {
+            self.cnc(response.cnc);
+            self.laser(response.laser);
+            self.showEntry(true);
+          } else {
+            self.allOff();
+          }
+        });
     };
 
     self.getJobInfo = function () {
@@ -84,11 +98,9 @@ $(function () {
         // console.log('status', o.state);
         console.log('Job Info:', o);
         if (o.state && o.state.indexOf("Offline") === 0) {
-          self.cnc(false);
-          self.laser(false);
-          self.showEntry(false);
+          self.allOff();
         }
-        self.fileLoaded(o.job.file.name != null);
+        self.fileLoaded(o);
         self.setFunctions();
       });
     };
@@ -106,8 +118,9 @@ $(function () {
     };
 
     //------- startup code -------------
+    self.attachControls();
+    self.allOff();
     self.setFunctions();
-    self.customControlsDiv = $("#control"); // add to this id's div (insert at bottom?)
     self.getSnapmakerInfo();
 
     console.log('This is my VM', self);
@@ -123,6 +136,6 @@ $(function () {
     // ViewModels your plugin depends on, e.g. loginStateViewModel, settingsViewModel, ...
     dependencies: ["connectionViewModel", "navigationViewModel", "settingsViewModel"],
     // Elements to bind to, e.g. #settings_plugin_snapmaker_control, #tab_plugin_snapmaker_control, ...
-    elements: ["#snapmaker_menu", "snapmaker_controls" /* ... */]
+    elements: ["#snapmaker_menu" /* ... */]
   });
 });
